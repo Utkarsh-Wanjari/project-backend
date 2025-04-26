74ceea892cd53261c9d991244b6ff11127b34fb9
Utkarsh_Wanjari <utkarshwanjari13@gmail.com>
2025-04-26 16:19:08 +0530

    Create pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any 
+    stages {
+        stage('code-pull'){
+            steps {
+                git branch: 'dev', url: 'https://github.com/Utkarsh-Wanjari/project-backend.git'
+            }
+        }
+        stage('code-Build'){
+            steps {
+               sh 'mvn clean package'
+            }
+        }
+         stage('Deploy-K8s'){
+            steps {
+               sh '''
+                    docker build . -t utkarsh1313/project-backend-img:latest
+                    docker push utkarsh1313/project-backend-img:latest
+                    docker rmi utkarsh1313/project-backend-img:latest
+                    kubectl apply -f ./deploy/
+
+               '''
+            }
+        }
+    }
+}
